810a7d74c28eb45f07d5752919e3a05f99f09314
Dmytro <dima.efik.ua@gmail.com>
2017-02-05 22:13:26 +0200

    homework 9-10

---
+++ javascript/9-10/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(document).ready(function() {
+
+// $(".js-demo-1").find("input").ionCheckRadio();
+
+$('select').niceSelect();
+
+$('.dropdown').hover( function (event) {
+
+   $(this).children('.sub_menu').slideDown(350);
+   $(this).children('.sub_menu').css('background', 'rgba(14, 23, 145, 0.5)');
+
+},
+   function () {
+      $(this).children('.sub_menu').slideUp(350);   
+   });
+
+
+});
+//  resize window and manipulate with class;
